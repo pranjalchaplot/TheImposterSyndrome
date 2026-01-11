0189e1f220947c9f1e4454aed0a7eedb1ec8b32c
Pranjal Chaplot <pranjalchaplot@gmail.com>
2026-01-11 23:58:52 +0530

    add data

--- services/wordBank.ts
+++ services/wordBank.ts
@@ -6,43 +6,169 @@ export interface CategoryMap {
 export const WORD_BANK: CategoryMap = {
   "Kitchen": [
     "Toaster", "Blender", "Refrigerator", "Microwave", "Dishwasher", "Oven", "Kettle", "Spatula", 
-    "Frying Pan", "Knife Block", "Cutting Board", "Sink", "Coffee Maker", "Spice Rack"
+    "Frying Pan", "Knife Block", "Cutting Board", "Sink", "Coffee Maker", "Spice Rack", "Mixer", 
+    "Peeler", "Whisk", "Bowl", "Plate", "Spoon", "Fork", "Cup", "Mug", "Apron", "Chef Hat", 
+    "Recipe", "Pantry", "Freezer", "Stove", "Trash Can", "Dish Soap", "Sponge", "Towel", "Grater", 
+    "Colander", "Ladle", "Tongs", "Rolling Pin", "Cookie Cutter", "Muffin Tin", "Baking Sheet"
   ],
   "School": [
     "Blackboard", "Chalk", "Desk", "Textbook", "Backpack", "Pencil Case", "Teacher", "Principal", 
-    "Homework", "Recess", "Library", "Cafeteria", "Gymnasium", "School Bus"
+    "Homework", "Recess", "Library", "Cafeteria", "Gymnasium", "School Bus", "Whiteboard", "Marker", 
+    "Eraser", "Ruler", "Scissors", "Glue", "Notebook", "Folder", "Locker", "Playground", "Janitor", 
+    "Nurse", "Student", "Classmate", "Test", "Exam", "Report Card", "Grade", "Subject", "Math", 
+    "Science", "History", "Art", "Music", "PE"
   ],
   "Beach": [
     "Sandcastle", "Sunscreen", "Surfboard", "Towel", "Umbrella", "Seagull", "Crab", "Ocean", 
-    "Lifeguard", "Volleyball", "Ice Cream", "Flip Flops", "Shell", "Bucket"
+    "Lifeguard", "Volleyball", "Ice Cream", "Flip Flops", "Shell", "Bucket", "Sand", "Sea", "Wave", 
+    "Sun", "Cloud", "Sky", "Bird", "Fish", "Boat", "Ship", "Yacht", "Jet Ski", "Surfer", "Swimmer", 
+    "Diver", "Snorkel", "Goggles", "Fins", "Wetsuit", "Bikini", "Swimsuit", "Trunks", "Hat", 
+    "Sunglasses", "Cooler", "Picnic", "Ball", "Frisbee", "Kite"
   ],
   "Space": [
     "Astronaut", "Rocket", "Moon", "Sun", "Black Hole", "Satellite", "Meteor", "Alien", 
-    "Spacesuit", "Telescope", "Galaxy", "Mars", "Space Station", "Gravity"
+    "Spacesuit", "Telescope", "Galaxy", "Mars", "Space Station", "Gravity", "Star", "Planet", 
+    "Comet", "Asteroid", "Nebula", "Universe", "Milky Way", "Earth", "Jupiter", "Saturn", "Uranus", 
+    "Neptune", "Pluto", "Mercury", "Venus", "Solar System"
   ],
   "Hospital": [
     "Doctor", "Nurse", "Stethoscope", "Wheelchair", "Ambulance", "Bandage", "Syringe", "X-Ray", 
-    "Patient", "Surgery", "Waiting Room", "Thermometer", "Crutches", "Medicine"
+    "Patient", "Surgery", "Waiting Room", "Thermometer", "Crutches", "Medicine", "Bed", "Stretcher", 
+    "IV Drip", "Mask", "Gloves", "Gown", "Scrubs", "Scalpel", "Needle", "Pill", "Tablet", "Capsule", 
+    "Syrup", "Ointment", "Cream", "Band-Aid", "Cast", "Paramedic", "Surgeon", "Dentist"
   ],
   "Cinema": [
     "Popcorn", "Ticket", "Screen", "Projector", "3D Glasses", "Seat", "Trailer", "Actor", 
-    "Director", "Usher", "Soda", "Candy", "Credits", "Red Carpet"
+    "Director", "Usher", "Soda", "Candy", "Credits", "Red Carpet", "Movie", "Film", "Show", "Row", 
+    "Aisle", "Exit", "Entrance", "Lobby", "Counter", "Nachos", "Hot Dog", "Pizza", "Burger", "Fries", 
+    "Slushie", "Drink", "Cup", "Straw", "Napkin", "Tray"
   ],
   "Supermarket": [
     "Trolley", "Cashier", "Barcode Scanner", "Vegetables", "Freezer", "Receipt", "Basket", 
-    "Aisle", "Checkout", "Paper Bag", "Discount", "Shelf", "Dairy", "Bakery"
+    "Aisle", "Checkout", "Paper Bag", "Discount", "Shelf", "Dairy", "Bakery", "Cart", "Bag", "Box", 
+    "Can", "Jar", "Bottle", "Pack", "Carton", "Tub", "Tube", "Roll", "Slice", "Piece", "Loaf", 
+    "Bunch", "Head", "Stalk", "Root", "Bulb", "Clove", "Kernel", "Grain", "Seed", "Nut", "Berry", 
+    "Fruit", "Meat", "Fish", "Poultry", "Deli", "Frozen"
   ],
   "Pirate Ship": [
     "Captain", "Parrot", "Treasure Chest", "Map", "Cannon", "Anchor", "Sail", "Eye Patch", 
-    "Peg Leg", "Sword", "Skull Flag", "Barrel", "Compass", "Plank"
+    "Peg Leg", "Sword", "Skull Flag", "Barrel", "Compass", "Plank", "Gold", "Silver", "Jewel", 
+    "Gem", "Diamond", "Ruby", "Emerald", "Sapphire", "Pearl", "Coin", "Doubloon", "Sack", "Pouch", 
+    "Wallet", "Purse", "Pocket", "Belt", "Sash", "Bandana", "Scarf", "Boots", "Coat", "Vest", 
+    "Shirt", "Pants"
   ],
   "Zoo": [
     "Lion", "Giraffe", "Elephant", "Penguin", "Zookeeper", "Cage", "Ticket Booth", "Monkey", 
-    "Snake", "Map", "Souvenir Shop", "Bear", "Zebra", "Fence"
+    "Snake", "Map", "Souvenir Shop", "Bear", "Zebra", "Fence", "Tiger", "Leopard", "Cheetah", 
+    "Jaguar", "Panther", "Cougar", "Puma", "Bobcat", "Lynx", "Ocelot", "Hyena", "Wolf", "Fox", 
+    "Coyote", "Jackal", "Dingo", "Polar Bear", "Panda", "Koala", "Kangaroo", "Wallaby", "Wombat"
   ],
   "Airport": [
     "Passport", "Suitcase", "Pilot", "Stewardess", "Security Check", "Runway", "Ticket", 
-    "Boarding Pass", "Duty Free", "Luggage Belt", "Gate", "Airplane", "Control Tower"
+    "Boarding Pass", "Duty Free", "Luggage Belt", "Gate", "Airplane", "Control Tower", "Plane", 
+    "Jet", "Helicopter", "Glider", "Drone", "Rocket", "Spaceship", "UFO", "Balloon", "Blimp", 
+    "Zeppelin", "Airship", "Parachute", "Hang Glider"
+  ],
+  "Sports": [
+    "Soccer", "Basketball", "Baseball", "Football", "Tennis", "Golf", "Cricket", "Rugby", "Hockey", 
+    "Volleyball", "Badminton", "Table Tennis", "Boxing", "Wrestling", "Judo", "Karate", "Kung Fu", 
+    "Taekwondo", "Gymnastics", "Swimming", "Diving", "Running", "Jogging", "Walking", "Cycling", 
+    "Biking", "Skating", "Skiing", "Snowboarding", "Surfing"
+  ],
+  "Music": [
+    "Guitar", "Piano", "Violin", "Cello", "Flute", "Clarinet", "Saxophone", "Trumpet", "Trombone", 
+    "Tuba", "Drums", "Percussion", "Bass", "Harp", "Banjo", "Ukulele", "Mandolin", "Sitar", 
+    "Accordion", "Harmonica", "Organ", "Synthesizer", "Keyboard", "Microphone", "Speaker", 
+    "Amplifier", "Headphones", "Earphones", "Radio", "CD"
+  ],
+  "Farm": [
+    "Cow", "Bull", "Calf", "Ox", "Buffalo", "Bison", "Yak", "Sheep", "Lamb", "Goat", "Kid", 
+    "Pig", "Hog", "Boar", "Sow", "Piglet", "Horse", "Mare", "Stallion", "Foal", "Pony", "Donkey", 
+    "Mule", "Chicken", "Rooster", "Hen", "Chick", "Duck", "Goose", "Turkey"
+  ],
+  "Circus": [
+    "Clown", "Acrobat", "Juggler", "Magician", "Tamer", "Ringmaster", "Trapeze", "Tightrope", 
+    "Unicycle", "Stilts", "Fire Eater", "Sword Swallower", "Contortionist", "Strongman", "Cannon", 
+    "Tent", "Ticket", "Popcorn", "Cotton Candy", "Balloon", "Animal", "Lion", "Tiger", "Elephant", 
+    "Bear", "Monkey", "Horse", "Dog", "Seal"
+  ],
+  "Office": [
+    "Computer", "Laptop", "Monitor", "Keyboard", "Mouse", "Printer", "Scanner", "Copier", "Fax", 
+    "Phone", "Mobile", "Tablet", "Stapler", "Staples", "Clip", "Pin", "Tack", "Tape", "Glue", 
+    "Scissors", "Knife", "Ruler", "Pen", "Pencil", "Marker", "Highlighter", "Eraser", "Sharpener", 
+    "Paper", "Notebook"
+  ],
+  "Camping": [
+    "Tent", "Sleeping Bag", "Mat", "Mattress", "Pillow", "Blanket", "Sheet", "Fire", "Wood", "Log", 
+    "Stick", "Match", "Lighter", "Torch", "Flashlight", "Lantern", "Lamp", "Stove", "Pot", "Pan", 
+    "Kettle", "Cup", "Mug", "Plate", "Bowl", "Spoon", "Fork", "Knife"
+  ],
+  "Christmas": [
+    "Santa", "Claus", "Elf", "Reindeer", "Sleigh", "Gift", "Present", "Box", "Bag", "Sack", 
+    "Stocking", "Tree", "Ornament", "Decoration", "Light", "Star", "Angel", "Bell", "Candle", 
+    "Wreath", "Mistletoe", "Holly", "Ivy", "Snow", "Snowflake", "Snowman", "Ice", "Winter", "Cold", 
+    "Frost"
+  ],
+  "Halloween": [
+    "Ghost", "Spirit", "Soul", "Phantom", "Specter", "Wraith", "Ghoul", "Zombie", "Mummy", 
+    "Vampire", "Werewolf", "Witch", "Wizard", "Warlock", "Sorcerer", "Magician", "Demon", "Devil", 
+    "Monster", "Beast", "Creature", "Alien", "Robot", "Cyborg", "Android", "Skeleton", "Skull", 
+    "Bone", "Grave", "Tomb"
+  ],
+  "Transportation": [
+    "Car", "Bus", "Truck", "Van", "Jeep", "Bike", "Motorcycle", "Scooter", "Skateboard", 
+    "Rollerblades", "Train", "Tram", "Subway", "Metro", "Monorail", "Boat", "Ship", "Ferry", 
+    "Yacht", "Canoe", "Kayak", "Raft", "Plane", "Jet", "Helicopter", "Rocket", "Taxi", "Uber", 
+    "Lyft", "Limousine"
+  ],
+  "Weather": [
+    "Sun", "Moon", "Star", "Cloud", "Rain", "Snow", "Hail", "Sleet", "Mist", "Fog", "Smog", 
+    "Wind", "Breeze", "Gale", "Storm", "Thunder", "Lightning", "Tornado", "Hurricane", "Cyclone", 
+    "Typhoon", "Monsoon", "Drought", "Flood", "Tsunami", "Earthquake", "Volcano", "Eruption", 
+    "Fire", "Heat"
+  ],
+  "Location (India)": [
+    "Taj Mahal", "Red Fort", "Qutub Minar", "Gateway of India", "Hawa Mahal", "India Gate", "Golden Temple", 
+    "Charminar", "Victoria Memorial", "Lotus Temple", "Mysore Palace", "Meenakshi Temple", "Konark Sun Temple", 
+    "Ajanta Caves", "Ellora Caves", "Dal Lake", "Goa Beaches", "Kerala Backwaters", "Varanasi Ghats", 
+    "Leh Ladakh", "Sundarbans", "Rann of Kutch", "Shimla", "Manali", "Darjeeling", "Ooty", "Munnar", 
+    "Jaipur", "Udaipur", "Jaisalmer"
+  ],
+  "Location (Global)": [
+    "Eiffel Tower", "Statue of Liberty", "Great Wall of China", "Pyramids of Giza", "Colosseum", "Machu Picchu", 
+    "Christ the Redeemer", "Sydney Opera House", "Burj Khalifa", "Grand Canyon", "Niagara Falls", "Mount Everest", 
+    "Stonehenge", "Petra", "Acropolis", "Santorini", "Venice Canals", "Louvre Museum", "Times Square", 
+    "Central Park", "Golden Gate Bridge", "Big Ben", "London Eye", "Vatican City", "Mount Fuji", 
+    "Great Barrier Reef", "Amazon Rainforest", "Sahara Desert", "Antarctica", "Galapagos Islands"
+  ],
+  "Actors (India)": [
+    "Shah Rukh Khan", "Amitabh Bachchan", "Salman Khan", "Aamir Khan", "Rajinikanth", "Kamal Haasan", 
+    "Akshay Kumar", "Hrithik Roshan", "Ranbir Kapoor", "Ranveer Singh", "Deepika Padukone", "Priyanka Chopra", 
+    "Alia Bhatt", "Kareena Kapoor", "Katrina Kaif", "Aishwarya Rai", "Madhuri Dixit", "Sridevi", "Kajol", 
+    "Rani Mukerji", "Nawazuddin Siddiqui", "Irrfan Khan", "Manoj Bajpayee", "Pankaj Tripathi", 
+    "Ayushmann Khurrana", "Vicky Kaushal", "Shahid Kapoor", "Ajay Devgn", "Saif Ali Khan", "Dilip Kumar"
+  ],
+  "Actors (Global)": [
+    "Leonardo DiCaprio", "Brad Pitt", "Tom Cruise", "Johnny Depp", "Robert Downey Jr.", "Dwayne Johnson", 
+    "Will Smith", "Denzel Washington", "Tom Hanks", "Morgan Freeman", "Meryl Streep", "Julia Roberts", 
+    "Angelina Jolie", "Scarlett Johansson", "Jennifer Lawrence", "Emma Stone", "Nicole Kidman", "Cate Blanchett", 
+    "Viola Davis", "Sandra Bullock", "Robert De Niro", "Al Pacino", "Marlon Brando", "Jack Nicholson", 
+    "Anthony Hopkins", "Daniel Day-Lewis", "Christian Bale", "Heath Ledger", "Joaquin Phoenix", "Keanu Reeves"
+  ],
+  "Events (India)": [
+    "Independence Day 1947", "Republic Day 1950", "Jallianwala Bagh Massacre", "Dandi March", "Quit India Movement", 
+    "Partition of India", "1983 Cricket World Cup", "2011 Cricket World Cup", "Kargil War", "1971 Indo-Pak War", 
+    "Green Revolution", "White Revolution", "Chipko Movement", "Pokhran Nuclear Tests", "Chandrayaan Mission", 
+    "Mangalyaan Mission", "Demonetization", "GST Launch", "Article 370 Abrogation", "Ayodhya Ram Mandir", 
+    "Swachh Bharat Abhiyan", "Make in India", "Digital India", "Kumbh Mela", "Asian Games 1982", 
+    "Commonwealth Games 2010"
+  ],
+  "Events (Global)": [
+    "World War I", "World War II", "Moon Landing", "Fall of Berlin Wall", "9/11 Attacks", "Titanic Sinking", 
+    "French Revolution", "American Revolution", "Industrial Revolution", "Cold War", "Vietnam War", 
+    "Civil Rights Movement", "Apartheid End", "Hiroshima Bombing", "Chernobyl Disaster", "Internet Invention", 
+    "Wright Brothers Flight", "Discovery of America", "Magna Carta", "Renaissance", "Black Death", "Spanish Flu", 
+    "COVID-19 Pandemic", "Olympics First Modern Games", "United Nations Formation", "EU Formation", "Brexit"
   ]
 };
 
